7c2cb5224b472045b2b326df280e839949af1c1b
Abhi-7-github <abhikollepara333@gmail.com>
2026-02-07 10:49:05 +0530

    Configure deployment for GitHub Pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/roseday/",
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
+              return 'vendor';
+            }
+            if (id.includes('framer-motion')) {
+              return 'framer-motion';
+            }
+            return 'utils'; // other dependencies
+          }
+        },
+      },
+    },
+  },
 })
